6d23b11b559c5f6cc29a3bc3351de56982fa29e5
Noah Citron <noah@jeff.org>
2022-10-11 05:41:53 -0400

    remove unused import

--- miner/worker.go
+++ miner/worker.go
@@ -39,7 +39,6 @@ import (
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
 )
 
